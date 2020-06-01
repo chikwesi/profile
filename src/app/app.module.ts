ffdd97bdf1055806ae37af240cc74238fa5f8564
chikwesi <kwesitor@gmail.com>
2020-06-01 12:31:01 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AskingComponent } from './asking/asking.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AskingComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
